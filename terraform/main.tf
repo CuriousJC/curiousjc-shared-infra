9accd6fdfd981258d915043615e62a9b4cc07956
Justin Crosby <justin.crosby@gmail.com>
2023-11-02 13:36:15 -0400

    using the actual username/password variables instead of hardcoding

--- terraform/main.tf
+++ terraform/main.tf
@@ -92,9 +92,9 @@ resource "aws_db_instance" "curiousjcdb" {
   instance_class         = "db.t2.micro" # Free Tier-eligible instance type
   db_name                = "curiousjcdb"
   identifier             = "curiousjcdb-instance"
-  username               = "xoladmin"    #todo: var.rds_admin
-  password               = "CzHq1sgh_Gx" #todo: var.rds_admin_password
-  skip_final_snapshot    = true          # Prevents a final DB snapshot when the instance is deleted
+  username               = var.rds_admin
+  password               = var.rds_admin_password
+  skip_final_snapshot    = true # Prevents a final DB snapshot when the instance is deleted
   vpc_security_group_ids = [aws_security_group.curiousjcdb_access_sg.id]
   db_subnet_group_name   = aws_db_subnet_group.curiousjcdb_subnet.name
   multi_az               = false
