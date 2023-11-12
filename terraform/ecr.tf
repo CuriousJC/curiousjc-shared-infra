64850cf7378bc6c3da43d674c3b6656e0ac33a14
Justin Crosby <justin.crosby@gmail.com>
2023-11-12 18:15:18 -0500

    adding some policies and changing my ECR name

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,6 +1,54 @@
 # Create an Amazon ECR repository
-resource "aws_ecr_repository" "my_ecr_repository" {
+resource "aws_ecr_repository" "curiousjc_ecr_repository" {
   name = "curiousjc-container-registry"
+}
+
+resource "aws_ecr_lifecycle_policy" "curiousjc_ecr_repository_untagged_policy" {
+  repository = aws_ecr_repository.curiousjc_ecr_repository.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 14 days",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 14
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_ecr_lifecycle_policy" "curiousjc_ecr_repository_tagged_policy" {
+  repository = aws_ecr_repository.curiousjc_ecr_repository.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 10 images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["v"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 10
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
 }
 
 # Output the ECR repository URL
